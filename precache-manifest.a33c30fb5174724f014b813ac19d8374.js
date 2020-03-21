1847c2dd892c1c8e620f664e5dc34a150f855e8f
paraline <fviktor1005@gmail.com>
2020-03-22 00:01:03 +0500

    Updates

---
+++ precache-manifest.a33c30fb5174724f014b813ac19d8374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429b7d13496de2fb2ed71e842bdbc962",
+    "url": "/sber-cur/index.html"
+  },
+  {
+    "revision": "184bb127c4a523b1e540",
+    "url": "/sber-cur/static/css/main.e133ff2e.chunk.css"
+  },
+  {
+    "revision": "2ad2363277380d306de1",
+    "url": "/sber-cur/static/js/2.fb382575.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/sber-cur/static/js/2.fb382575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "184bb127c4a523b1e540",
+    "url": "/sber-cur/static/js/main.de6e5cc5.chunk.js"
+  },
+  {
+    "revision": "99a846b8f9712bc417f0",
+    "url": "/sber-cur/static/js/runtime-main.a0a8adc0.js"
+  }
+]);
